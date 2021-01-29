2cda01141f5b50958a92184f7eace7abe31a6736
Samuel Lee <samuelxlee@gmail.com>
2021-01-28 23:59:29 -0800

    wrote tests for the register endpoint

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,46 @@
-// Write your tests here
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true);
+});
+
+const request = require("supertest");
+const db = require("../data/dbConfig");
+const server = require("./server");
+
+const sammy = { username: "sammy", password: "1234" };
+const jimmy = { username: "jimmy", password: "5678" };
+
+beforeAll(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+});
+
+beforeEach(async () => {
+  await db("users").truncate();
+});
+
+afterAll(async () => {
+  await db.destroy();
 })
+
+describe("server", () => {
+  describe("[POST] /api/register", () => {
+    it("responds with 201 Created", async () => {
+      const res = await request(server).post("/api/auth/register").send(sammy);
+      expect(res.status).toBe(201);
+    })
+    it("responds with the newly created user", async () => {
+      let res;
+      res = await request(server).post("/api/auth/register").send(sammy);
+      expect(res.body.username).toBe("sammy");
+
+      res = await request(server).post("/api/auth/register").send(jimmy);
+      expect(res.body.username).toBe("jimmy");
+    })
+  });
+  describe("[GET] /api/jokes", () => {
+    it("responds with 200 OK", async () => {
+      const res = await request(server).get("/api/jokes");
+      expect(res.status).toBe(200);
+    });
+  });
+});
